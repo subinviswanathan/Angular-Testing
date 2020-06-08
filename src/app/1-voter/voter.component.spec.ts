75831e41d0a33238c06a6d17cb69b00861bbbadf
subinviswanathan <subinv1992@gmail.com>
2020-06-08 14:46:54 +0530

    voter intergration testing

--- src/app/1-voter/voter.component.spec.ts
+++ src/app/1-voter/voter.component.spec.ts
@@ -1,5 +1,6 @@
 import { VoterComponent } from './voter.component';
 import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 describe('VoterComponent', () => {
   let component: VoterComponent;
@@ -16,6 +17,25 @@ describe('VoterComponent', () => {
     // fixture.debugElement
   });
 
-  it('', () => {
+  it('should render total votes', () => {
+    component.othersVote = 20;
+    component.myVote = 1;
+    fixture.detectChanges();
+
+    let de = fixture.debugElement.query(By.css('.vote-count'));
+    let el: HTMLElement = de.nativeElement;
+
+    expect(el.innerText).toContain('21');
+
+  });
+
+  it('should highlight the upvote button if I have upvoted', () => {
+    component.myVote = 1;
+    fixture.detectChanges();
+
+    let de = fixture.debugElement.query(By.css('.glyphicon-menu-up'));
+
+    expect(de.classes['highlighted']).toBeTruthy();
+
   });
 });
