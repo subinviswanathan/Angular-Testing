1ad78527c6e2aea248dd9eb63457b2641f09e309
subinviswanathan <subinv1992@gmail.com>
2020-06-08 14:21:24 +0530

    spy for services for add and delete and confirm dialog

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -1,12 +1,72 @@
-import { TodosComponent } from './todos.component'; 
-import { TodoService } from './todo.service'; 
+import { TodosComponent } from './todos.component';
+import { TodoService } from './todo.service';
+import { from, empty, throwError } from 'rxjs';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
+  let service: TodoService;
 
   beforeEach(() => {
+    service = new TodoService(null);
+    component = new TodosComponent(service);
   });
 
-  it('', () => {
+  it('should set todos property with items returned from the server', () => {
+    let todos = [1, 2, 3];
+    spyOn(service, 'getTodos').and.callFake(() => from([todos]));
+
+    component.ngOnInit();
+
+    expect(component.todos.length).toBe(3);
+    expect(component.todos).toBe(todos);
   });
+
+  it('should call the server to save the changes when a new todo item is added', () => {
+    let spy = spyOn(service, 'add').and.callFake(t => empty());
+
+    component.add();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should add the new todo returned from the server', () => {
+    let todo = { id: 1 }
+    //spyOn(service, 'add').and.callFake(t => from([todo]));
+    spyOn(service, 'add').and.returnValue(from([todo]));
+
+    component.add();
+
+    expect(component.todos.includes(todo)).toBeTruthy();
+
+  });
+
+  it('should set the message property if server returns an error', () => {
+    let error = 'Error from Server';
+    //spyOn(service, 'add').and.callFake(t => from([todo]));
+    spyOn(service, 'add').and.returnValue(throwError(error));
+
+    component.add();
+
+    expect(component.message).toBe(error);
+
+  });
+
+  it('should call the server to delete the todo item if the user confirms', () => {
+    spyOn(window, 'confirm').and.returnValue(true);
+    let spy = spyOn(service, 'delete').and.returnValue(empty());
+
+    component.delete(1);
+
+    expect(spy).toHaveBeenCalledWith(1);
+  });
+
+  it('should NOT call the server to delete the todo item if the user cancels', () => {
+    spyOn(window, 'confirm').and.returnValue(false);
+    let spy = spyOn(service, 'delete').and.returnValue(empty());
+
+    component.delete(1);
+
+    expect(spy).not.toHaveBeenCalledWith(1);
+  });
+
 });
