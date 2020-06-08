397cbd1eca061bb40aa34739f191514630b5626a
subinviswanathan <subinv1992@gmail.com>
2020-06-08 13:41:17 +0530

    set up and tear down

--- src/app/03-setup-and-teardown/vote.component.spec.ts
+++ src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,41 @@
-import { VoteComponent } from './vote.component'; 
+import { VoteComponent } from './vote.component';
 
 describe('VoteComponent', () => {
-  it('', () => {
+
+  let component: VoteComponent;
+
+  // set up
+  beforeEach(() => {
+    component = new VoteComponent();
   });
 
-  it('', () => {
+  beforeAll(() => { });
+
+  // tear down -> clean up code.
+  afterEach(() => { })
+
+  afterAll(() => { });
+
+  it('should increment totalVotes when upVoted', () => {
+    // Arrange.
+    // let component = new VoteComponent();
+
+    // Act.
+    component.upVote();
+
+    // Assertion.
+    expect(component.totalVotes).toBe(1);
+
+  });
+
+  it('should decrement totalVotes when downVoted', () => {
+    // Arrange.
+    // let component = new VoteComponent();
+
+    // Act.
+    component.downVote();
+
+    // Assertion.
+    expect(component.totalVotes).toBe(-1);
   });
 });
